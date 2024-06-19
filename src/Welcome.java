8d8e70e728a842d8ece04145e8a901805be47cb7
vege4944 <vege4944@gmail.com>
2024-06-19 09:46:37 +0900

    Welcome First Commit

---
+++ src/Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome {
+
+    //실행매소드가필요하니 main 메소드 입력
+    public static void main(String[] args) {
+        System.out.println("********************************");//출력명령어
+        System.out.println("\tWelcome to Shopping Mall\t"); //탭만큼 띄우라는소리=escape sequence
+        System.out.println("\tWelcome to Book Market!\t");
+        System.out.println("********************************");//ctrl+y 한줄지우기
+        System.out.println(" 1. 고객 정보 확인하기        4. 바구니에 항목 추가하기");
+        System.out.println(" 1. 고객 정보 확인하기        4. 바구니에 항목 추가하기");
+        System.out.println("********************************");
+    }//cli=command line 콘솔에 출력프로그램
+    //항상위에부터 아래로 1차원적으로 순서대로 나열되고 실행된다
+}
